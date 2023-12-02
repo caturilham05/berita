ad9f76a89f491ed1947e61cf4de26809f3491c6d
catur <catur@fisip.net>
2023-12-03 02:48:55 +0700

    uppercase class mdel

---
+++ app/Models/Contents.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contents extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'tag_ids',
+        'cat_ids',
+        'title',
+        'intro',
+        'image',
+        'image_thumb',
+        'images',
+        'content',
+        'is_active',
+        'timestamp',
+        'url',
+    ];
+    public $timestamps = false;
+}
